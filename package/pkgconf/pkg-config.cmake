70f65655d7457251d20db86d7688bdc3494f2178
Ningyuan Li <mariotaku.lee@gmail.com>
2023-06-26 00:25:15 +0900

    fixing pkg-config install

--- package/pkgconf/pkg-config.cmake
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env cmake
-
-set(STAGING_SUBDIR "${GNU_TARGET_NAME}/sysroot")
-set(STATIC "")
-
-configure_file(${BR_SOURCE_DIR}/package/pkgconf/pkg-config.in ${HOST_DIR}/bin/pkg-config @ONLY)
