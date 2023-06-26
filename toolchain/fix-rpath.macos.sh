b7e69cae8b7974bbdff1563dbfc526523d4d7007
Ningyuan Li <mariotaku.lee@gmail.com>
2023-06-27 01:11:48 +0900

    added fix-rpath script

---
+++ toolchain/fix-rpath.macos.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ ! -d "$1" ]; then
+  echo "Usage: $0 LIBDIR"
+  exit 1
+fi
+
+LIBDIR=$(realpath "$1")
+AWK_LIB_PREFIX=$(echo "$LIBDIR" | sed -e 's/\//\\\//g')
+
+find bin lib libexec -type file | while read -r FILE; do
+  if [ $(file -b --mime-type "${FILE}") != "application/x-mach-binary" ]; then
+    continue
+  fi
+
+  HAS_CHANGED=false
+  while read LINK; do
+    LINK_NAME=$(basename "${LINK}")
+    if [ -z "${LINK}" ] || [ $(basename "$FILE") = "${LINK_NAME}" ]; then
+      continue
+    fi
+    install_name_tool -change "${LINK}" "@rpath/${LINK_NAME}" "${FILE}"
+    HAS_CHANGED=true
+  done <<<"$(otool -LX ${FILE} | awk /${AWK_LIB_PREFIX}/'{ print $1 }')"
+
+  if [ "$HAS_CHANGED" = true ]; then
+    if [ $(dirname "${FILE}") != "lib" ]; then
+      install_name_tool -add_rpath "@loader_path/$(dirname ${FILE} | sed -e 's/[^/]*/../g')/lib" "${FILE}"
+    fi
+    echo "Patched ${FILE}"
+  fi
+done
